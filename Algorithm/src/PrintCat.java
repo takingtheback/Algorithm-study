4dbfc267cda0f2e4b21f93819ec808a683a8324f
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-21 20:08:39 +0900

    Create PrintCat.java
    고양이 출력하기
    - 이클립스 폰트가 Pretendard 사용중
    - | 이 겹쳐서 보이는 문제로 코드 계속 오류 발생함

---
+++ Algorithm/src/PrintCat.java
@@ -0,0 +1,12 @@
+
+public class PrintCat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
