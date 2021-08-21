76202f4cb3100875635900fee8fe1a9ff8d1410b
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-21 20:57:29 +0900

    Create Multiple.java

---
+++ Algorithm/src/Multiple.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiple {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+		in.close();
+		System.out.println(A*(B%10));
+		System.out.println(A*(B%100/10));
+		System.out.println(A*(B/100));
+		System.out.println(A*B);
+	}
+ 
+}
