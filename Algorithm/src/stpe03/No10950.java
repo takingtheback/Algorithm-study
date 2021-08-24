d53af32c46ac964387e9548bf971bb7ddfc9d62b
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-24 18:07:41 +0900

    Create No10950.java

---
+++ Algorithm/src/stpe03/No10950.java
@@ -0,0 +1,29 @@
+package stpe03;
+
+import java.util.Scanner;
+
+/**
+ * 문제
+ * A+B - 3
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ * 입력
+ * 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 출력
+ * 각 테스트 케이스마다 A+B를 출력한다.
+ */
+
+public class No10950 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int len = scan.nextInt();
+		
+		for(int i = 0; i < len; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
