36695b89dc98e1932e8cffd0429b610a5d79fbc4
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-08-24 17:45:44 +0900

    Create gugudan.java

---
+++ Algorithm/src/stpe03/gugudan.java
@@ -0,0 +1,18 @@
+package stpe03;
+
+import java.util.Scanner;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int dan = scan.nextInt();
+		
+		for (int i = 1; i< 10;i++) {
+			System.out.println(dan + " * "+ i + " = " + (dan*i));
+		}
+	}
+}
+
+
