802a69cd729ac5d933646c2648c0f6a0e64ed1af
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-09-04 15:56:48 +0900

    Create No2742.java

---
+++ Algorithm/src/stpe03/No2742.java
@@ -0,0 +1,27 @@
+package stpe03;
+
+import java.util.Scanner;
+
+public class No2742 {
+	/**
+	 * 문제
+	 * 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+	 * 
+	 * 입력
+	 * 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+	 * 출력
+	 * 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		scan.close();
+        
+		while(N > 0) {
+			System.out.println(N);
+			N--;
+		}
+	}
+}
+ 
