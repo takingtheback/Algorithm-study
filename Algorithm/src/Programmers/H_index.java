b74a76cea66de18449aeec48f78444570fdf6713
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-09-05 15:03:46 +0900

    Create H_index.java

---
+++ Algorithm/src/Programmers/H_index.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class H_index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+ 
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+ 
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+ 
+        return answer;
+    }
+}
