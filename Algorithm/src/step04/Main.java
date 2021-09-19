86e970f39b8c84824df9ed2bc85e7d37fe9ae786
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-09-19 11:43:24 +0900

    Create Main.java

---
+++ Algorithm/src/step04/Main.java
@@ -0,0 +1,22 @@
+package step04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		
+		while(true) {
+		
+			int A=in.nextInt();
+			int B=in.nextInt();
+		
+			if(A==0 && B==0){
+				in.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+}
