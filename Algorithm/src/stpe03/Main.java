03254f3e844a691bfa26ff37fe98026ef8b27a28
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-09-20 15:25:39 +0900

    Update Main.java

--- Algorithm/src/stpe03/Main.java
+++ Algorithm/src/stpe03/Main.java
@@ -3,15 +3,23 @@ package stpe03;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		int dan = scan.nextInt();
-		
-		for (int i = 1; i< 10;i++) {
-			System.out.println(dan + " * "+ i + " = " + (dan*i));
+		 
+		int N = scan.nextInt();
+		int X = scan.nextInt();
+		int arr[] = new int[N];
+        
+		for (int i = 0; i < N; i++) {
+			arr[i] = scan.nextInt();
+		}
+ 
+		scan.close();
+        
+		for (int i = 0; i < N; i++) {
+			if (arr[i] < X) {
+				System.out.print(arr[i] + " ");
+			}
 		}
 	}
 }
-
