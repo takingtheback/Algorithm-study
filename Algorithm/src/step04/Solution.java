9eb81c9da8ecb75784d0faa07415ddd49c4a0644
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-10-04 16:40:26 +0900

    Create Solution.java

---
+++ Algorithm/src/step04/Solution.java
@@ -0,0 +1,12 @@
+package step04;
+
+public class Solution {
+    public String solution(String code, String message) {
+    	code = "abcdefghijklmnopqrstuvwxyz";
+		message = "test";
+    	String answer = "";
+	 
+    	
+        return "";
+    }
+}
